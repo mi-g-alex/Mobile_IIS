a65344600f107dd52a011baba4396e1627478d78
sirxvj <bekman2004@mail.ru>
2023-04-07 00:03:24 +0300

    а вот еще файлы забыл

---
+++ app/src/main/java/by/g_alex/mobile_iis/presentation/grade_book_screen/GradebookScreen.kt
@@ -0,0 +1,10 @@
+package by.g_alex.mobile_iis.presentation.grade_book_screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun RatingScreen(){
+
+}
