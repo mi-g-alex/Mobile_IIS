db6eb9f7b49cbea0c76ae41fb4d97460929350cd
G_Alex <sashagorgun@yandex.com>
2023-05-22 00:56:35 +0300

    миграции 2

--- app/src/main/java/by/g_alex/mobile_iis/data/local/UserDataBase.kt
+++ app/src/main/java/by/g_alex/mobile_iis/data/local/UserDataBase.kt
@@ -3,7 +3,12 @@ package by.g_alex.mobile_iis.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import by.g_alex.mobile_iis.data.local.entity.*
+import by.g_alex.mobile_iis.data.local.entity.CookieEntity
+import by.g_alex.mobile_iis.data.local.entity.GradeBookEntity
+import by.g_alex.mobile_iis.data.local.entity.LessonModel
+import by.g_alex.mobile_iis.data.local.entity.LoginAndPasswordEntity
+import by.g_alex.mobile_iis.data.local.entity.MarkBookEntity
+import by.g_alex.mobile_iis.data.local.entity.ProfilePersonalCVEntity
 
 
 @Database(
